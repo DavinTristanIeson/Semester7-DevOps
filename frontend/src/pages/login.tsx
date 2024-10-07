9db88badca3c14776a91b14a1e1f77dad2281e73
Hansen Tanio <hansentanio33@gmail.com>
2024-10-07 12:19:32 +0700

    style: login page and register page styling

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -4,9 +4,9 @@ import { useRouter } from "next/router";
 import NavigationRoutes from "@/common/constants/routes";
 import Link from "next/link";
 import AuthenticationForm from "@/modules/auth/form";
-import { Box, Title } from "@mantine/core";
+import { Box, lighten, Title } from "@mantine/core";
 
-import LoginStyles from "@/modules/auth/login.module.css";
+import AuthStyles from "@/styles/auth.module.css";
 import Colors from "@/common/constants/colors";
 
 export default function LoginPage() {
@@ -14,21 +14,38 @@ export default function LoginPage() {
   const router = useRouter();
 
   return (
-    <Box p={16} className={LoginStyles["login--card"]}>
-      <Title order={1}>Login</Title>
-      <AuthenticationForm
-        onSubmit={async (values) => {
-          const res = await login(values);
-          if (res.message) {
-            showNotification({
-              message: res.message,
-              color: Colors.Sentiment.Info
-            });
-          }
-          router.replace(NavigationRoutes.Home);
-        }}
-      />
-      <Link href={NavigationRoutes.Register}>Register</Link>
+    <Box className={AuthStyles["full-screen-box"]}>
+      <Box className={AuthStyles["container-box"]}>
+        <Box className={AuthStyles["login-image-box"]}>
+          <Title order={1} style={{ fontFamily: "monospace" }}>PARALLEL</Title>
+          <Box className={AuthStyles["quote-box"]}>
+            <Title order={2}>Capturing Moments,</Title>
+            <Title order={2}>Creating Memories</Title>
+          </Box>
+        </Box>
+        <Box className={AuthStyles["content-box"]}>
+          <Title order={1}>Sign In</Title>
+          <AuthenticationForm
+            onSubmit={async (values) => {
+              const res = await login(values);
+              if (res.message) {
+                showNotification({
+                  message: res.message,
+                  color: Colors.Sentiment.Info,
+                });
+              }
+              router.replace(NavigationRoutes.Home);
+            }}
+          />
+          <text>Don't have an account?
+            <span>
+              <Link href={NavigationRoutes.Register} className={AuthStyles["link-style"]}>
+                Register
+              </Link>
+            </span>
+          </text>
+        </Box>
+      </Box>
     </Box>
   );
 }
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -6,27 +6,51 @@ import Link from "next/link";
 import AuthenticationForm from "@/modules/auth/form";
 import { Box, Title } from "@mantine/core";
 import Colors from "@/common/constants/colors";
+import AuthStyles from "@/styles/auth.module.css";
+import { TextInput } from "@mantine/core";
 
 export default function RegisterPage() {
   const { mutateAsync: register } = useRegister();
   const router = useRouter();
 
   return (
-    <Box p={16}>
-      <Title order={1}>Register</Title>
-      <AuthenticationForm
-        onSubmit={async (values) => {
-          const res = await register(values);
-          if (res.message) {
-            showNotification({
-              message: res.message,
-              color: Colors.Sentiment.Info
-            });
-          }
-          router.replace(NavigationRoutes.Home);
-        }}
-      />
-      <Link href={NavigationRoutes.Login}>Login</Link>
+    <Box className={AuthStyles["full-screen-box"]}>
+      <Box className={AuthStyles["container-box"]}>
+        <Box className={AuthStyles["register-image-box"]}>
+          <Title order={1} style={{ fontFamily: "monospace" }}>PARALLEL</Title>
+          <Box className={AuthStyles["quote-box"]}>
+            <Title order={2}>Capturing Moments,</Title>
+            <Title order={2}>Creating Memories</Title>
+          </Box>
+        </Box>
+        <Box className={AuthStyles["content-box"]}>
+          <Title order={1}>Create an Account</Title>
+          <TextInput
+            label="Name"
+            placeholder="Enter Name"
+          />
+          <AuthenticationForm
+            onSubmit={async (values) => {
+              const res = await register(values);
+              if (res.message) {
+                showNotification({
+                  message: res.message,
+                  color: Colors.Sentiment.Info
+                });
+              }
+              router.replace(NavigationRoutes.Home);
+            }}
+          />
+          <text>Already have account?
+            <span>
+              <Link href={NavigationRoutes.Login} className={AuthStyles["link-style"]}>
+                Login
+              </Link>
+            </span>
+          </text>
+
+        </Box>
+      </Box>
     </Box>
   );
 }
