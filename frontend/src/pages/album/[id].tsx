b859a64527bae961f9e070ca96a93278ca9ca623
Hansen Tanio <hansentanio33@gmail.com>
2024-10-07 12:57:37 +0700

    style: gallery styling (home page)

--- frontend/src/pages/album/[id].tsx
+++ frontend/src/pages/album/[id].tsx
@@ -1,5 +1,6 @@
-import { Button, FileInput, Flex, Title } from "@mantine/core";
+import { Box, Button, FileInput, Flex, Title } from "@mantine/core";
 import { useRouter } from "next/router";
+import AlbumStyles from "@/styles/album.module.css";
 
 export default function AlbumViewPage() {
   const router = useRouter();
@@ -10,6 +11,44 @@ export default function AlbumViewPage() {
         <FileInput label="Album Files" multiple />
         <Button>Upload</Button>
       </Flex>
+      <Box className={AlbumStyles["gallery"]}>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Login.jpg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Example2.png" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Example1.jpeg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Register.jpg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Login.jpg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Example2.png" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Example1.jpeg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Register.jpg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Login.jpg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Example2.png" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Example1.jpeg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+        <Box className={AlbumStyles["album"]}>
+          <img src="/images/Register.jpg" alt="Login Image" className={AlbumStyles["cover-image"]} />
+        </Box>
+      </Box>
     </div>
   );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,25 +3,108 @@ import { SessionToken } from "@/common/auth/token";
 import Colors from "@/common/constants/colors";
 import NavigationRoutes from "@/common/constants/routes";
 import { handleErrorFn } from "@/common/utils/form";
-import { Button, Title } from "@mantine/core";
+import { Box, Button, Title } from "@mantine/core";
 import { showNotification } from "@mantine/notifications";
 import { useRouter } from "next/router";
+import NavbarStyles from "@/styles/navbar.module.css";
+import HomeStyles from "@/styles/home.module.css";
 
 export default function HomePage() {
   const { mutateAsync: logout, isPending } = useLogout();
   const router = useRouter();
-  return <div>
-    <Title order={1}>Hello world</Title>
-    <Button loading={isPending} onClick={handleErrorFn(async () => {
-      const res = await logout();
-      if (res.message){
-        showNotification({
-          message: res.message,
-          color: Colors.Sentiment.Info,
-        })
-      }
-      router.replace(NavigationRoutes.Login);
-      SessionToken.clear();
-    })}>Logout</Button>
-  </div>;
+  return (
+    <div>
+      <Box className={NavbarStyles["navbar"]}>
+        <Title order={1} style={{ fontFamily: "monospace" }}>
+          PARALLEL
+        </Title>
+        <Button
+          className={NavbarStyles["logout-button"]}
+          loading={isPending}
+          onClick={handleErrorFn(async () => {
+            const res = await logout();
+            if (res.message) {
+              showNotification({
+                message: res.message,
+                color: Colors.Sentiment.Info,
+              });
+            }
+            router.replace(NavigationRoutes.Login);
+            SessionToken.clear();
+          })}
+        >
+          Logout
+        </Button>
+      </Box>
+      <Box className={HomeStyles["gallery"]}>
+        <Box className={HomeStyles["album"]}>
+          <Box className={HomeStyles["cover"]}>
+            <img src="/images/Login.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example2.png" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Register.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example1.jpeg" alt="Login Image" className={HomeStyles["cover-image"]} />
+          </Box>
+          <Title order={1} style={{ fontFamily: "monospace" }}>
+            Album 1
+          </Title>
+        </Box>
+        <Box className={HomeStyles["album"]}>
+          <Box className={HomeStyles["cover"]}>
+            <img src="/images/Login.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example2.png" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Register.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example1.jpeg" alt="Login Image" className={HomeStyles["cover-image"]} />
+          </Box>
+          <Title order={1} style={{ fontFamily: "monospace" }}>
+            Album 2
+          </Title>
+        </Box>
+        <Box className={HomeStyles["album"]}>
+          <Box className={HomeStyles["cover"]}>
+            <img src="/images/Login.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example2.png" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Register.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example1.jpeg" alt="Login Image" className={HomeStyles["cover-image"]} />
+          </Box>
+          <Title order={1} style={{ fontFamily: "monospace" }}>
+            Album 3
+          </Title>
+        </Box>
+        <Box className={HomeStyles["album"]}>
+          <Box className={HomeStyles["cover"]}>
+            <img src="/images/Login.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example2.png" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Register.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example1.jpeg" alt="Login Image" className={HomeStyles["cover-image"]} />
+          </Box>
+          <Title order={1} style={{ fontFamily: "monospace" }}>
+            Album 4
+          </Title>
+        </Box>
+        <Box className={HomeStyles["album"]}>
+          <Box className={HomeStyles["cover"]}>
+            <img src="/images/Login.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example2.png" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Register.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example1.jpeg" alt="Login Image" className={HomeStyles["cover-image"]} />
+          </Box>
+          <Title order={1} style={{ fontFamily: "monospace" }}>
+            Album 5
+          </Title>
+        </Box>
+        <Box className={HomeStyles["album"]}>
+          <Box className={HomeStyles["cover"]}>
+            <img src="/images/Login.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example2.png" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Register.jpg" alt="Login Image" className={HomeStyles["cover-image"]} />
+            <img src="/images/Example1.jpeg" alt="Login Image" className={HomeStyles["cover-image"]} />
+          </Box>
+          <Title order={1} style={{ fontFamily: "monospace" }}>
+            Album 6
+          </Title>
+        </Box>
+
+      </Box>
+    </div>
+  );
 }
